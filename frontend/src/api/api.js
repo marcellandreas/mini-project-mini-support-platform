4c3b5ef2006c09e268d2ad721dcdd2110d420526
Marcell Andreas Samadhani Duha <marcellandreas.123@gmail.com>
2025-09-20 13:03:14 +0700

    change: api url

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000",
+  baseURL: "https://mini-project-mini-support-platform-amber.vercel.app",
 });
 
 export default API;
